e99b223bcff04cbb30698adfd95fa0eeefcf645d
杨朝辉 <15729355390@139.com>
2020-05-18 08:48:04 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/Android/db/City.java
@@ -0,0 +1,50 @@
+package com.coolweather.Android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+
+    private int id;
+
+//    市名
+    private String cityName;
+
+//    市代号
+    private int cityCode;
+
+//    当前市所属省的id
+    private int provinceId;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
